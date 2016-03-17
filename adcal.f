4ecf420768e4d444cc460aa8832436fad0e8735a
SayakaO <typical.33.egoist33@gmail.com>
2016-03-17 14:24:22 +0900

    econ(k)=eco(k)+tbal*dt*qeco(k)に変更

--- adcal.f
+++ adcal.f
@@ -168,7 +168,7 @@ c
 c       difez=(fl-fu)/ddz(k)
 c
 c       econ(k)=eco(k)+tbal*dt*(-advez+difez+qeco(k))
-        econ(k)=eco(k)+qeco(k)*dt
+        econ(k)=eco(k)+tbal*dt*qeco(k)
 c
    10 continue
 c
